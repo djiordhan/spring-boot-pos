b87451a520c4fd7b480a657da4d4a742bdd497b9
jordan <jordandiamante@icloud.com>
2024-05-21 20:20:35 +0800

    update package

--- src/main/java/com/djiordhan/pos/Main.java
+++ src/main/java/com/djiordhan/pos/Main.java
@@ -1,4 +1,4 @@
-package com.djiordhan.demo;
+package com.djiordhan.pos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
